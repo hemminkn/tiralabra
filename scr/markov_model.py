c2e5ee6871fb81d31608d2062d088299f5a981e3
hemminkn <nooralotta.hemminki@gmail.com>
2026-09-15 13:49:28 +0300

    function can now read multiple training files

--- scr/markov_model.py
+++ scr/markov_model.py
@@ -2,20 +2,27 @@ import sys
 import random
 from collections import defaultdict, Counter
 from sequences import get_sequence
+from pathlib import Path
 
 class MarkovChain:
-	def __init__(self, filename, order):
-		self.filename = filename
+	def __init__(self, key, order):
+		if key == "Minors":
+			self.files = Path("../midi/Minors").glob("*.mid")
+		else:
+			self.files = Path("../midi/Majors").glob("*.mid")
 		self.order = order
 		self.model = defaultdict(Counter)
 
 	def train(self):
-		notes = get_sequence(self.filename)
 
-		for i in range(len(notes)-self.order):
-			state = tuple(notes[i:i+self.order])
-			next_note = notes[i+self.order]
-			self.model[state][next_note]+=1
+		for file in self.files:
+			notes = get_sequence(file)
+
+			for i in range(len(notes)-self.order):
+				state = tuple(notes[i:i+self.order])
+				next_note = notes[i+self.order]
+
+				self.model[state][next_note]+=1
 
 		return(self.model)
 

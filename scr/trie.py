877c1fa4363e6acbd02f3b8d420b9c8f0261364c
hemminkn <nooralotta.hemminki@gmail.com>
2026-09-19 13:13:32 +0300

    finished find_next function

--- scr/trie.py
+++ scr/trie.py
@@ -1,14 +1,15 @@
 from collections import Counter
+import sys
 
-Class TrieNode:
+class TrieNode:
     def __init__(self):
         self.children = {}
         self.frequency = 0
         self.next_notes = Counter()
 
-Class Trie:
+class Trie:
     def __init__(self):
-        self.root = Trienode()
+        self.root = TrieNode()
 
     def insert(self, state, next_note):
         current = self.root
@@ -19,7 +20,30 @@ Class Trie:
 
             current = current.children[note]
 
+            current.frequency += 1
+
         current.next_notes[next_note] += 1
 
-    def find_next():
-        pass
+    def find_next(self, state):
+        current = self.root
+
+        for note in state:
+            if note in current.children:
+                current = current.children[note]
+            else:
+                return ([], [])
+
+        successors = []
+        weights = []
+
+        for note in current.next_notes:
+            successors.append(note)
+            weights.append(current.next_notes[note])
+
+        return (successors, weights)
+
+if __name__ == "__main__":
+    trie = Trie()
+    trie.insert((60, 62), 64)
+    trie.insert((61, 63), 65)
+    print(trie.find_next((60, 62)))

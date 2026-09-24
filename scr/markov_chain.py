60a160f0eea399ec6359b639bbf74addeba26040
hemminkn <nooralotta.hemminki@gmail.com>
2026-09-24 11:48:41 +0300

    renamed file and little fixes

--- scr/markov_chain.py
+++ scr/markov_chain.py
@@ -1,27 +1,31 @@
 import sys
 from sequences import get_sequence
 from pathlib import Path
-import trie
+from trie import Trie
 
-class Train:
+class MarkovChain:
 	def __init__(self, key, order):
+		self.trie = Trie() #alustetaan trie, eli t = Train("Minors", 2) -> t.trie
 		if key == "Minors":
 			self.files = Path("../midi/Minors").glob("*.mid")
 		else:
 			self.files = Path("../midi/Majors").glob("*.mid")
 		self.order = order
 
-	def organize_data(self):
+	def train(self):
 
 		for file in self.files:
 			notes = get_sequence(file)
+
+			start_state = tuple(notes[:self.order])
+			self.trie.start_states.append(start_state)
 
 			for i in range(len(notes)-self.order):
 				state = tuple(notes[i:i+self.order])
 				next_note = notes[i+self.order]
 
-				trie.insert(state, next_note)
+				self.trie.insert(state, next_note)
 
 if __name__ == "__main__":
-	t = Train(sys.argv[1], 2)
-	print(t.organize_data())
+	m = MarkovChain(sys.argv[1], 2)
+	print(m.train())

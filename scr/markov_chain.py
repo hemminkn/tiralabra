322eeb4ae7e41f3eaaf4f6cee984486e2c670976
hemminkn <nooralotta.hemminki@gmail.com>
2026-09-24 12:55:18 +0300

    made the code neater

--- scr/markov_chain.py
+++ scr/markov_chain.py
@@ -6,14 +6,14 @@ from trie import Trie
 class MarkovChain:
 	def __init__(self, key, order):
 		self.trie = Trie() #alustetaan trie, eli t = Train("Minors", 2) -> t.trie
+		self.order = order
+
 		if key == "Minors":
 			self.files = Path("../midi/Minors").glob("*.mid")
 		else:
 			self.files = Path("../midi/Majors").glob("*.mid")
-		self.order = order
 
 	def train(self):
-
 		for file in self.files:
 			notes = get_sequence(file)
 
@@ -25,7 +25,3 @@ class MarkovChain:
 				next_note = notes[i+self.order]
 
 				self.trie.insert(state, next_note)
-
-if __name__ == "__main__":
-	m = MarkovChain(sys.argv[1], 2)
-	print(m.train())

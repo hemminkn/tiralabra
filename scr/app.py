438cac8765dc63683e56c19534d4c3dee8d2c067
hemminkn <nooralotta.hemminki@gmail.com>
2026-09-24 12:52:10 +0300

    added general testing

--- scr/app.py
+++ scr/app.py
@@ -1,4 +1,7 @@
+from markov_chain import MarkovChain
+from generator import generate
 from flask import Flask, render_template
+import sys
 
 app = Flask(__name__)
 
@@ -6,6 +9,11 @@ app = Flask(__name__)
 @app.route("/index")
 def index():
     return render_template("index.html", title="Generator")
+
+#testing
+m = MarkovChain("Minors", 2)
+m.train()
+generate(m.trie)
 
 if __name__ == "__main__":
     app.run(debug=True)
